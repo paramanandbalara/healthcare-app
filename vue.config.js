1dca6622196d544555c8c97241e18a5ee337b53b
Paramanand Balara <115616408+paramanand-shypmax@users.noreply.github.com>
2023-10-18 15:51:30 +0530

    path update

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
 	transpileDependencies: true,
-	publicPath: '/healthcare-app/'
+	publicPath: '/'
 	// publicPath: process.env.NODE_ENV === 'production' ? '/healthcare-app/' : '/'
 });
 
